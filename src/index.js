be9bbd4f71be11f6d73bded0452a79230c9e0ed3
margaridamfonseca <164942212+margaridamfonseca@users.noreply.github.com>
2024-03-28 18:13:29 +0000

    Added Real day and time

--- src/index.js
+++ src/index.js
@@ -9,8 +9,23 @@ function weatherUpdate(response){
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     let windSpeedElement = document.querySelector("#windSpeed");
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    timeElement.innerHTML = formatDate(date);
+
+
 }
 
+function formatDate(date){
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+   return `${day} ${hours}:${minutes}`; 
+}
 
 function searchCity(city){
     let apiKey ="8dod4a3bd4def17f159b978bb700cbt9"
