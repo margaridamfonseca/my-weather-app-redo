0195848d0c17d62f3c245a8969a887474c1edb98
margaridamfonseca <164942212+margaridamfonseca@users.noreply.github.com>
2024-03-28 17:02:15 +0000

    Added Temperature update and City update with API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
+function weatherUpdate(response){
+    let temperatureElement = document.querySelector("#weather-app-temperature-value");
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    let cityElement = document.querySelector("h1")
+    cityElement.innerHTML = response.data.city;
+}
+
+
+function searchCity(city){
+    let apiKey ="8dod4a3bd4def17f159b978bb700cbt9"
+    let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric` 
+    axios.get(apiUrl).then(weatherUpdate);
+}
+
 function provideCity(event){
     event.preventDefault();
     let searchInputElement = document.querySelector("#form-search-box")
-    let cityElement = document.querySelector("h1")
-    cityElement.innerHTML = searchInputElement.value
+    searchCity(searchInputElement.value)
 }
 
 
 
 let searchFormElement = document.querySelector("#search-form")
 searchFormElement.addEventListener("submit", provideCity)
+
+searchCity("Lisbon")
